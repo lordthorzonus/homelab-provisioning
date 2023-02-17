731a4cb1a2bdb37f859a6e33d028b8c38b3776f9
Juuso Leinonen <juuso.leinonen@wunderdog.fi>
2023-02-18 00:59:39 +0200

    Fix bugs found when setupping the cluster from scratch

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source = "Telmate/proxmox"
-      version = "2.9.13"
+      version = "2.9.11"
     }
   }
   required_version = " ~>1.3.8"
